7851bbca44f1fd388c95a7a8576bf1482206c37a
lovecoding-1982 <faststdb@163.com>
2022-06-07 17:59:16 +0800

    init hash

---
+++ faststdb/common/hash.cc
@@ -0,0 +1,15 @@
+/*!
+ * \file hash.cc
+ */
+#ifndef FASTSTDB_COMMON_HASH_H_
+#define FASTSTDB_COMMON_HASH_H_
+
+#include "faststdb/common/hash.h"
+
+namespace faststdb {
+namespace common {
+
+}  // namespace common
+}  // namespace faststdb
+
+#endif  // FASTSTDB_COMMON_HASH_H_
